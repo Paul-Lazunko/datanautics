10bc56be071fc28b531c19cb69881ce7597287cf
pashaman <plavel@gmail.com>
2026-02-12 20:36:54 +0700

    feat: updated tests

--- test/test.cjs
+++ test/test.cjs
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 const { Datanautics } = require('../dist');
 const { setTimeout } = require('timers');
 
-const dumpPath = resolve(__dirname, './data.txt');
+const pathToDumpFile = resolve(__dirname, './data.txt');
 const data = {
   user: { firstname: 'John Junior Frost 11', lastname: 'Doe' },
   score: [27],
@@ -17,8 +17,8 @@ const data = {
   kk: [],
 };
 async function main() {
-  const writer = new Datanautics({ dumpPath, writer: true, verbose: true });
-  const reader = new Datanautics({ dumpPath, writer: false, verbose: true });
+  const writer = new Datanautics({ pathToDumpFile, writer: true, verbose: true });
+  const reader = new Datanautics({ pathToDumpFile, writer: false, verbose: true });
   await writer.init();
   await reader.init();
   assert.equal(writer.set('user.firstname', data.user.firstname), true);
@@ -27,11 +27,18 @@ async function main() {
   assert.equal(writer.set('nested', data.nested), true);
   assert.equal(writer.set('j', data.j), true);
   assert.equal(writer.set('kk', data.kk), true);
-
+  await writer.store();
+  assert.equal(writer.set('user.firstname', data.user.firstname), true);
+  assert.equal(writer.set('user.lastname', data.user.lastname), true);
+  assert.equal(writer.set('score[0]', data.score[0]), true);
+  assert.equal(writer.set('nested', data.nested), true);
+  assert.equal(writer.set('j', data.j), true);
+  assert.equal(writer.set('kk', data.kk), true);
   assert.equal(writer.get('user.firstname'), data.user.firstname);
   assert.equal(writer.get('user.lastname'), data.user.lastname);
   assert.equal(writer.get('score[0]'), data.score[0]);
-
+  assert.equal(writer.get('kk'), data.kk);
+  await writer.store();
   setTimeout(() => {
     assert.equal(reader.get('user.firstname'), data.user.firstname);
     assert.equal(reader.get('user.lastname'), data.user.lastname);
