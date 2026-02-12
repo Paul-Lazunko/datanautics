f5b1379d8d60fa41563d597b9179c5df2e4b7440
pashaman <plavel@gmail.com>
2026-02-12 11:19:15 +0700

    feat: updated test to cover new edge cases

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ const data = {
   user: { firstname: 'John Junior Frost 11', lastname: 'Doe' },
   score: [27],
   nested: { key: BigInt('1000000000000000000000') },
+  j: [],
 };
 
 const writer = new Datanautics({ dumpPath, writer: true, verbose: true, dumpInterval: 1000 });
@@ -17,7 +18,8 @@ const reader = new Datanautics({ dumpPath, writer: false });
 assert.equal(writer.set('user.firstname', data.user.firstname), true);
 assert.equal(writer.set('user.lastname', data.user.lastname), true);
 assert.equal(writer.set('score[0]', data.score[0]), true);
-assert.equal(writer.set('user.nested', data.nested), true);
+assert.equal(writer.set('nested', data.nested), true);
+assert.equal(writer.set('j', data.j), true);
 
 assert.equal(writer.get('user.firstname'), data.user.firstname);
 assert.equal(writer.get('user.lastname'), data.user.lastname);
@@ -27,6 +29,7 @@ setTimeout(() => {
   assert.equal(reader.get('user.firstname'), data.user.firstname);
   assert.equal(reader.get('user.lastname'), data.user.lastname);
   assert.equal(reader.get('score[0]'), data.score[0]);
+  assert.equal(JSON.stringify(reader.get('j')), JSON.stringify(data.j));
   console.log('tests passed');
   process.exit(0);
 }, 2000);
