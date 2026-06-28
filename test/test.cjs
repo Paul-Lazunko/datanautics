51f371b09cf83daef554673ca946eee475db432f
pashaman <plavel@gmail.com>
2026-06-28 11:41:54 +0700

    feat: updated tests

--- test/test.cjs
+++ test/test.cjs
@@ -2,9 +2,18 @@ const assert = require('assert');
 const { resolve } = require('path');
 
 const { Datanautics } = require('../dist');
-const { setTimeout } = require('timers');
+const { createWriteStream, createReadStream } = require('fs');
 
 const pathToDumpFile = resolve(__dirname, './data.txt');
+
+const writeStream = createWriteStream(pathToDumpFile, {
+  flags: 'w',
+  encoding: 'utf8',
+  autoClose: true,
+});
+
+const readStream = createReadStream(pathToDumpFile, { encoding: 'utf8' });
+
 const data = {
   user: { firstname: 'John Junior Frost 11', lastname: 'Doe' },
   score: [27],
@@ -18,16 +27,14 @@ const data = {
 };
 async function main() {
   const writer = new Datanautics({ pathToDumpFile, writer: true, verbose: true });
-  const reader = new Datanautics({ pathToDumpFile, writer: false, verbose: true });
-  await writer.init();
-  await reader.init();
+  await writer.init(readStream);
   assert.equal(writer.set('user.firstname', data.user.firstname), true);
   assert.equal(writer.set('user.lastname', data.user.lastname), true);
   assert.equal(writer.set('score[0]', data.score[0]), true);
   assert.equal(writer.set('nested', data.nested), true);
   assert.equal(writer.set('j', data.j), true);
   assert.equal(writer.set('kk', data.kk), true);
-  await writer.store();
+  await writer.store(writeStream);
   assert.equal(writer.set('user.firstname', data.user.firstname), true);
   assert.equal(writer.set('user.lastname', data.user.lastname), true);
   assert.equal(writer.set('score[0]', data.score[0]), true);
@@ -38,16 +45,8 @@ async function main() {
   assert.equal(writer.get('user.lastname'), data.user.lastname);
   assert.equal(writer.get('score[0]'), data.score[0]);
   assert.equal(writer.get('kk'), data.kk);
-  await writer.store();
-  setTimeout(() => {
-    assert.equal(reader.get('user.firstname'), data.user.firstname);
-    assert.equal(reader.get('user.lastname'), data.user.lastname);
-    assert.equal(reader.get('score[0]'), data.score[0]);
-    assert.equal(JSON.stringify(reader.get('j')), JSON.stringify(data.j));
-    assert.equal(JSON.stringify(reader.get('kk')), JSON.stringify(data.kk));
-    console.log('tests passed');
-    process.exit(0);
-  }, 0);
+  console.log('OK');
+  process.exit(0);
 }
 
 main().catch(console.error);
